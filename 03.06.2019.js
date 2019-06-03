83fca3fd51e5e9ffdea05fbdc447cbfca7d47078
dnagafonov <denisagafonovwork.gmail.com>
2019-06-04 02:31:05 +0300

    Update 03.06.2019.js

--- 03.06.2019.js
+++ 03.06.2019.js
@@ -27,7 +27,7 @@ function removeChar(str){
 removeChar(str);
 
 //task4
-var arr = [1,4,5,-67,8,78];
+var arr = [1,4,5,2];
 function min(arr){
 	return console.log("t3", Math.min.apply(null,arr));
 }
@@ -42,7 +42,7 @@ function expandedForm(num) {				//error
 		arr.push(temp*(Math.pow(10,i-1)));
 		temp = 0;
 	}
-	return console.log(arr[0]);
+	return console.log("t5",arr[0]);
 }
 expandedForm(335);
 
@@ -50,5 +50,14 @@ expandedForm(335);
 function opposite(number) {
   return number *= -1;
 opposite(-23);
+}
 
-
+//task7
+var arr1 = [1,4,5,2];
+function squareSum(arr1){
+	for (var i = 0; i != arr1.length - 1; i--) {
+		var temp = 0;
+		temp += Math.pow.(arr1[i],2);
+	}
+	return console.log(temp);
+}
